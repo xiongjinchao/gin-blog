cccecde9bffc4a5571f9a06bf1364d76b74d7be0
xiongjinchao <xiongjinchao@hotmail.com>
2019-10-17 00:01:55 +0800

    carry on

--- controllers/blog/home.go
+++ controllers/blog/home.go
@@ -1,9 +1,6 @@
 package blog
 
 import (
-	"fmt"
-	db "gin-admin/database"
-	"gin-blog/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -12,19 +9,27 @@ type Home struct{}
 
 // Index handles GET /admin route
 func (h *Home) Index(c *gin.Context) {
-	var menus, data []models.Menu
-	data, err := (&models.Menu{}).GetCache()
-	if err != nil {
-		if err := db.Mysql.Model(&models.Menu{}).Find(&menus).Error; err != nil {
-			_, _ = fmt.Fprintln(gin.DefaultWriter, err.Error())
+	/*
+		data, err := db.Redis.Get("menu").Result()
+		fmt.Println(data)
+		fmt.Println(err)
+
+	*/
+
+	/*
+		var menus, data []models.Menu
+		data, err := (models.Menu{}).GetCache()
+		if err != nil {
+			if err := db.Mysql.Model(&models.Menu{}).Find(&menus).Error; err != nil {
+				_, _ = fmt.Fprintln(gin.DefaultWriter, err.Error())
+			}
+			(&models.Menu{}).SetSort(&menus, 0, &data)
+			(&models.Menu{}).SetSpace(&data)
 		}
-		(&models.Menu{}).SetSort(&menus, 0, &data)
-		(&models.Menu{}).SetSpace(&data)
-	}
-	fmt.Println(data)
+		fmt.Println(data)
+	*/
 
 	c.HTML(http.StatusOK, "home/index", gin.H{
 		"title": "首页",
-		"menu":  data,
 	})
 }
