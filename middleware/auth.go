eb07cc0170afbe34d9679abfabb84c3cb9e81527
xiongjinchao <xiongjinchao@funhan.cn>
2019-11-02 18:27:35 +0800

    carry on

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"gin-blog/helper"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -12,6 +13,8 @@ func (_ *Auth) CheckAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		auth := helper.GetUser(c)
+		fmt.Println(auth)
+		fmt.Println(auth.User.ID)
 
 		if auth.User.ID <= 0 {
 			c.Redirect(http.StatusFound, "/")
